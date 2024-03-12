7222c8aff91f1f6cf494d6fa68bcf595ad63950a
kenjayev <kenjayevazizbek2oo4@gmail.com>
2024-03-12 07:02:22 +0500

    full

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
 const menu_BTN = document.querySelector(".hamburger-menu"),
-    navigation = document.querySelector(".main-navigation");
+    navigation = document.querySelector(".main-navigation"),
+    headerLinks = document.querySelectorAll("header .main-navigation a");
+clock_box = document.querySelector(".clock");
 
 menu_BTN.addEventListener("click", () => {
     menu_BTN.classList.toggle("active");
     navigation.classList.toggle("active");
 });
+
+headerLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        menu_BTN.classList.remove("active");
+        navigation.classList.remove("active");
+    });
+});
+
+setInterval(() => {
+    let hour = new Date().getHours();
+    let minute = new Date().getMinutes();
+    minute = minute > 9 ? minute : "0" + minute;
+    clock_box.innerHTML = `${hour}:${minute}`;
+}, 1000);
